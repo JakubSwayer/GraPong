5efe37d9363fd8a568a79b404937da4eba3154a6
JakubSwayer <97314265+JakubSwayer@users.noreply.github.com>
2022-04-18 20:00:08 +0200

    Update wygra.js

--- wygra.js
+++ wygra.js
@@ -10,7 +10,14 @@ addEventListener("resize", function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  iW = innerWidth * 0.5 - 180;
+  iH = innerHeight * 0.35;
+
+  if (menu) {
+    createMenu();
+  }
 });
+var audio_win = new Audio('Win.wav');
 var audio = new Audio("pong.wav");
 var c = canvas.getContext('2d');
 
@@ -29,6 +36,7 @@ var menu_state = 1;
 var cpu_player = false;
 
 var animation;
+let nl = [];
 
 menu_title = new Image();
 menu_title.src = 'StartGame.png';
@@ -42,17 +50,22 @@ menu_player2.src = 'Player2.png';
 menu_indicator = new Image();
 menu_indicator.src = 'indicator.png';
 
+for (var i = 0; i < 5; i++) {
+nl[i] = new Image();
+nl[i].src = 'numbers/' + i + '.png';
+}
+
+
 
 
 function createMenu() {
 
-  var ind_position = 75 + (menu_state - 1)*50;
+  var ind_position = 75 + (menu_state - 1) * 50;
 
   c.drawImage(menu_title, iW, iH);
   c.drawImage(menu_player1, iW + 25, iH + 75);
   c.drawImage(menu_player2, iW + 25, iH + 125);
   c.drawImage(menu_indicator, iW, iH + ind_position);
-
 }
 
 
@@ -84,10 +97,10 @@ function Block(x, y) {
       list[1].y += v;
     }
 
-    if (cpu_player && pilka.x > innerWidth/2 && pilka.dx > 0) {
-      if(pilka.y < list[1].y + (player_height/2) && list[1].y > 0) {
+    if (cpu_player && pilka.x > innerWidth / 2 && pilka.dx > 0) {
+      if (pilka.y < list[1].y + (player_height / 2) && list[1].y > 0) {
         list[1].y -= 2;
-      } else if (pilka.y > list[1].y + (player_height/2) && list[1].y < innerHeight - player_height ) {
+      } else if (pilka.y > list[1].y + (player_height / 2) && list[1].y < innerHeight - player_height) {
         list[1].y += 2;
       }
     }
@@ -111,12 +124,12 @@ let pilka = {
   },
   update: function() {
 
-    if (this.x < 130 && this.x > 120 && this.y < (list[0].y + player_height + radius) && this.y > list[0].y - radius) {
+    if (this.x < 130 && this.x > 125 && this.y < (list[0].y + player_height + radius) && this.y > list[0].y - radius) {
       this.dx = -this.dx;
       this.dy = (Math.random() - 0.5) * 8;
       audio.play();
     }
-    if (this.x > innerWidth - 130 && this.x < innerWidth - 120 && this.y < (list[1].y + player_height + radius) && this.y > list[1].y - radius) {
+    if (this.x > innerWidth - 130 && this.x < innerWidth - 125 && this.y < (list[1].y + player_height + radius) && this.y > list[1].y - radius) {
       this.dx = -this.dx;
       this.dy = (Math.random() - 0.5) * 8;
       audio.play();
@@ -133,12 +146,18 @@ let pilka = {
 
     if (this.x <= 0) {
       punkty_2++;
+      if (punkty_2 != win) {
+        audio.play();
+      }
       this.x = (innerWidth / 2);
       this.y = (innerHeight / 2);
       this.dx = 5;
       this.dy = 0;
     } else if (this.x >= innerWidth) {
       punkty_1++;
+      if (punkty_1 != win) {
+        audio.play();
+      }
       this.x = (innerWidth / 2);
       this.y = (innerHeight / 2);
       this.dx = 5;
@@ -148,12 +167,12 @@ let pilka = {
     if (punkty_1 == win) {
       punkty_1 = 0;
       punkty_2 = 0;
-      audio.play();
+      audio_win.play();
     }
     if (punkty_2 == win) {
       punkty_1 = 0;
       punkty_2 = 0;
-      audio.play();
+      audio_win.play();
     }
 
 
@@ -183,17 +202,17 @@ window.onkeydown = function(event) {
     case "s":
       if (list[0].y < (innerHeight - player_height - 5)) {
         key_s = true;
-        }
+      }
       break;
     case "o":
-      if (list[1].y > 5  && !cpu_player) {
+      if (list[1].y > 5 && !cpu_player) {
         key_o = true;
-        }
+      }
       break;
     case "l":
       if (list[1].y < (innerHeight - player_height - 5) && !cpu_player) {
         key_l = true;
-        }
+      }
       break;
     case "p":
       pause++;
@@ -205,20 +224,22 @@ window.onkeydown = function(event) {
       break;
     case "ArrowUp":
       if (menu_state == 2) {
-        menu_state = 1; }
-        audio.play();
+        menu_state = 1;
+      }
+      audio.play();
       break;
     case "ArrowDown":
       if (menu_state == 1) {
-        menu_state = 2; }
-        audio.play();
-        break;
+        menu_state = 2;
+      }
+      audio.play();
+      break;
     case "Enter":
-    if(menu && menu_state == 1){
-      cpu_player = true;
-    }
-        menu = false;
-        break;
+      if (menu && menu_state == 1) {
+        cpu_player = true;
+      }
+      menu = false;
+      break;
   }
 };
 window.onkeyup = function(event) {
@@ -241,10 +262,8 @@ window.onkeyup = function(event) {
 
 
 function draw_outcome() {
-  c.font = "30px Arial";
-  c.strokeStyle = "#F55353";
-  c.strokeText(punkty_1, innerWidth / 2 - 80, 50);
-  c.strokeText(punkty_2, innerWidth / 2 + 20, 50);
+  c.drawImage(nl[punkty_1],innerWidth / 2 - 80, 50);
+  c.drawImage(nl[punkty_2], innerWidth / 2 + 20, 50);
 }
 
 
